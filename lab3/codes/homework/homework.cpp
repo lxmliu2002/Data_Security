9e5bdf57f52af2db5b9cec6e269bd9d2c18dcea7
lxmliu2002 <lxmliu2002@126.com>
2024-03-19 10:33:16 +0800

    fix(lab3): update lab3 codes and report

--- lab3/codes/homework/homework.cpp
+++ lab3/codes/homework/homework.cpp
@@ -8,22 +8,22 @@ int main()
 
     // 客户端的视角：要进行计算的数据
     vector<double> x, y, z;
-    x = { 1.0, 2.0, 3.0 };
-    y = { 2.0, 3.0, 4.0 };
-    z = { 3.0, 4.0, 5.0 };
-    cout<<"原始向量x是："<<endl;
+    x = {1.0, 2.0, 3.0};
+    y = {2.0, 3.0, 4.0};
+    z = {3.0, 4.0, 5.0};
+    cout << "原始向量x是：" << endl;
     print_vector(x);
-    cout<<"原始向量y是："<<endl;
+    cout << "原始向量y是：" << endl;
     print_vector(y);
-    cout<<"原始向量z是："<<endl;
+    cout << "原始向量z是：" << endl;
     print_vector(z);
-    cout<<endl;
+    cout << endl;
     // 构建参数容器 parms
     EncryptionParameters parms(scheme_type::ckks);
     // 这里的参数都使用官方建议的
     size_t poly_modulus_degree = 8192;
     parms.set_poly_modulus_degree(poly_modulus_degree);
-    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 60, 40, 40, 60 }));
+    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {60, 40, 40, 60}));
     double scale = pow(2.0, 40);
 
     // 用参数生成 CKKS 框架 context
@@ -56,13 +56,12 @@ int main()
     encryptor.encrypt(yp, yc);
     encryptor.encrypt(zp, zc);
 
-
     /*
     下面进入本次实验的核心内容
     计算$x^3+y*z$
     */
     // 步骤1，计算x^2
-        print_line(__LINE__);
+    print_line(__LINE__);
     cout << "计算 x^2 ." << endl;
     Ciphertext x2;
     evaluator.multiply(xc, xc, x2);
@@ -72,15 +71,15 @@ int main()
     // 然后查看一下此时x^2结果的level
     print_line(__LINE__);
     cout << " + Modulus chain index for x2: "
-<< context.get_context_data(x2.parms_id())->chain_index() << endl;
+         << context.get_context_data(x2.parms_id())->chain_index() << endl;
 
     // 步骤2，计算1.0*x
     // 此时xc本身的层级应该是2，比x^2高，因此这一步解决层级问题
     print_line(__LINE__);
     cout << " + Modulus chain index for xc: "
-<< context.get_context_data(xc.parms_id())->chain_index() << endl;
+         << context.get_context_data(xc.parms_id())->chain_index() << endl;
     // 因此，需要对 x 进行一次乘法和 rescaling操作
-        print_line(__LINE__);
+    print_line(__LINE__);
     cout << "计算 1.0*x ." << endl;
     Plaintext plain_one;
     encoder.encode(1.0, scale, plain_one);
@@ -90,22 +89,21 @@ int main()
     // 再次查看 xc 的层级，可以发现 xc 与 x^2 层级变得相同
     print_line(__LINE__);
     cout << " + Modulus chain index for xc new: "
-<< context.get_context_data(xc.parms_id())->chain_index() << endl;
+         << context.get_context_data(xc.parms_id())->chain_index() << endl;
     // 那么，此时xc与x^2层级相同，二者可以相乘了
 
     // 步骤3，计算x^3，即1*x*x^2
     // 先设置新的变量叫x3
-        print_line(__LINE__);
+    print_line(__LINE__);
     cout << "计算 1.0*x*x^2 ." << endl;
     Ciphertext x3;
     evaluator.multiply_inplace(x2, xc);
-    evaluator.relinearize_inplace(x2,relin_keys);
+    evaluator.relinearize_inplace(x2, relin_keys);
     evaluator.rescale_to_next(x2, x3);
     // 此时观察x^3的层级
     print_line(__LINE__);
-cout << " + Modulus chain index for x3: "
-<< context.get_context_data(x3.parms_id())->chain_index() << endl;
-
+    cout << " + Modulus chain index for x3: "
+         << context.get_context_data(x3.parms_id())->chain_index() << endl;
 
     // 步骤4，计算y*z
     print_line(__LINE__);
@@ -118,7 +116,7 @@ cout << " + Modulus chain index for x3: "
     // 然后查看一下此时y*z结果的level
     print_line(__LINE__);
     cout << " + Modulus chain index for yz: "
-<< context.get_context_data(yz.parms_id())->chain_index() << endl;
+         << context.get_context_data(yz.parms_id())->chain_index() << endl;
 
     // 注意，此时问题在于scales的不统一，可以直接重制。
     print_line(__LINE__);
@@ -137,10 +135,10 @@ cout << " + Modulus chain index for x3: "
     evaluator.mod_switch_to_inplace(yz, last_parms_id);
     print_line(__LINE__);
     cout << " + Modulus chain index for yz new: "
-<< context.get_context_data(yz.parms_id())->chain_index() << endl;
+         << context.get_context_data(yz.parms_id())->chain_index() << endl;
 
     // 步骤5，x^3+y*z
-        print_line(__LINE__);
+    print_line(__LINE__);
     cout << "计算 x^3+y*z ." << endl;
     Ciphertext encrypted_result;
     evaluator.add(x3, yz, encrypted_result);
@@ -154,7 +152,7 @@ cout << " + Modulus chain index for x3: "
     encoder.decode(result_p, result);
 
     // 输出结果
-        print_line(__LINE__);
+    print_line(__LINE__);
     cout << "结果是：" << endl;
     print_vector(result, 3 /*precision*/);
 
